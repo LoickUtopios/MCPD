7b22e10945b289c824a6da270d4bfb0a5f1df4c7
Loick <loick@utopios.solutions>
2026-02-16 16:39:59 +0100

    Demo 2 replace dans le dossier correspondant

---
+++ 08_ASP_MVC/Demos/Demo2_View/Demo2_View/Views/Home/Vue.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Demo2_View.Views.Home
+{
+    public class VueModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
