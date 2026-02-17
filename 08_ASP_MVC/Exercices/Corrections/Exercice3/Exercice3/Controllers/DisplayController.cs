9451d266fb9718c54e2d4ff3b6808913f9b639c3
Loick <loick@utopios.solutions>
2026-02-17 10:44:30 +0100

    Ajout de la correction de l'exercice 3

---
+++ 08_ASP_MVC/Exercices/Corrections/Exercice3/Exercice3/Controllers/DisplayController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Exercice3.Controllers
+{
+    public class DisplayController : Controller
+    {
+        public IActionResult Index()
+        {
+            string prenom = "toto";
+            int age = 15; 
+            List<string> hobbies = new List<string>() {"Jeux vidéo", "Cinéma", "Randonnée" };
+
+            ViewData["prenom"] = prenom;
+            ViewData["age"] = age;
+            ViewData["hobbies"] = hobbies;
+            return View();
+        }
+    }
+}
