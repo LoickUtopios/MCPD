d6e993cad84037ffb38bb69fd59f8651762f82e7
Loick <loick@utopios.solutions>
2026-02-18 11:41:31 +0100

    Correction de l'exo5

---
+++ 08_ASP_MVC/Exercices/Corrections/Exercice5/Exercice5/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Exercice5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercice5.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult DisplayForm()
+        {
+            return View(new Product());
+        }
+
+        [HttpPost]
+        public IActionResult TreatForm(Product product) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("DisplayForm", product);
+            }
+            return RedirectToAction("DisplayResult");
+        }
+
+        [HttpGet]
+        public IActionResult DisplayResult()
+        {
+            return View();
+        }
+    }
+}
---
+++ 08_ASP_MVC/Exercices/Corrections/Exercice5/Exercice5/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exercice5.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Le nom est obligatoire")]
+        [StringLength(255, ErrorMessage ="Le nom du produit doit être au maximum de 255 caractère")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Le prix est obligatoire")]
+        [Range(1,double.MaxValue, ErrorMessage ="Le prix doit être supérieur à 0")]
+        public double Price { get; set; }
+        [Required(ErrorMessage = "La categorie est obligatoire")]
+        [StringLength(255, ErrorMessage = "Le nom de la categorie doit être au maximum de 255 caractère")]
+        public string Category { get; set; }
+
+        public Product() { }
+        public Product(int id, string name, double price, string category)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Name} {Price} {Category}";
+        }
+    }
+}
